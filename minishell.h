841ba597a8c2d26757bdd28e7d70218153cc1598
tjdwns5063@naver.com <tjdwns5063@naver.com>
2021-12-14 17:23:16 +0900

    헤더 수정

--- minishell.h
+++ minishell.h
@@ -6,13 +6,29 @@
 /*   By: tkim <tkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 17:12:43 by tkim              #+#    #+#             */
-/*   Updated: 2021/12/14 17:13:33 by tkim             ###   ########.fr       */
+/*   Updated: 2021/12/14 17:22:42 by seongjki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
-#endif
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <errno.h>
+# include <stdio.h>
+
+typedef struct s_lst
+{
+	int 	node;
+	char	*str;
+	struct s_lst *next;
+}	t_lst;
+
+
+
+# endif
