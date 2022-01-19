dbc5f043095e59d6ea02e751ac7e0ea2f14c72f0
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2022-01-20 01:10:56 +0900

    #36 | 단위로 실행
    - 토큰리스트 단위에서 실행 -> 파이프 단위로 프로세스 실행 변경
    - 리다이렉션 합치는 과정에서 에러발견
    a - 1,2,3  b - 4,5,6
    cat < a > b | wc -l
    위 명령 실행시 b 에는 1,2,3 이 담기고 터미널엔 0 출력
    지금 상황은 wc -l 결과인 0이 b에 담겨져있음

--- src/exec_func.c
+++ src/exec_func.c
@@ -19,7 +19,10 @@ int	check_redirection(t_cmd *cmd, int *fd_in, int *fd_out)
 	curr = cmd;
 	if (curr->next_flag != REDIRECT1 && curr->next_flag != REDIRECT2 && curr->next_flag != REDIRECT3 && curr->next_flag != REDIRECT4)
 		return (0);
+	if(!cmd->next)
+		return (0);
 	file_name = cmd->next->argv[0];
+	fprintf(stderr,"file = %s\n",file_name);
 	if (curr->next_flag == REDIRECT1)
 		*fd_out = open(file_name, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	else if (curr->next_flag == REDIRECT2 || curr->next_flag == REDIRECT4)
@@ -33,34 +36,18 @@ int	check_redirection(t_cmd *cmd, int *fd_in, int *fd_out)
 
 int	exec_path(char *path, char *argv[], char *env_arr[], int *fd_in, int *fd_out)
 {
-	int			idx;
-	int			pid;
-	int			state;
-	int			path_arr_len;
-	int n;
-	int			ret;
+	int		ret;
 
-	idx = 0;
-	// pid = fork();
-	//if (pid > 0)
-	//	waitpid(pid, &state, 0);
-	//else if (pid == 0)
-	//{
-		// if (*fd_in != 0)
-		// 	dup2(*fd_in, 0);
-		// if (*fd_out != 1)
-		// 	dup2(*fd_out, 1);
-
-		ret = execve(path, argv, env_arr);
-		if (ret == -1)
-		{
-			printf("minishell42: %s: command not found\n", path);
-			//exit(1);
-		}
-	//}
-	//else if (pid < 0)
-	//exit(0);
-	// return (0);
+	//if (fd_in != 0)
+	//	dup2(fd_in, 0);
+	//if (fd_out != 0)
+	//	dup2(fd_out, 1);
+	ret = execve(path, argv, env_arr);
+	if (ret == -1)
+	{
+		printf("minishell42: %s: command not found\n", path);
+		exit(1);
+	}
 }
 
 int	exec_built_in_func(char *argv[], t_lst **env_lst)
--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 
 #include "minishell.h"
 
+int		is_redirection(int flag)
+{
+	if(flag == REDIRECT1 || flag == REDIRECT2 ||flag == REDIRECT3 ||flag == REDIRECT4)
+		return (1);
+	return (0);
+}
+
 void	free_memory(char **str)
 {
 	int	idx;
@@ -25,6 +32,7 @@ int	exec_func(t_cmd *cmd, t_lst **env_lst)
 	int status;
 	char	*path;
 	char	**path_arr;
+	t_cmd *curr;
 
 	if (!cmd)
 		return (1);
@@ -46,26 +54,39 @@ int	exec_func(t_cmd *cmd, t_lst **env_lst)
 		fork()
 		exec_funfc()
 	}*/
+	// printf("next = %d\n",cmd->next_flag);
 	while(cmd)
 	{
 		path_arr = path_parsing(cmd->argv[0], *env_lst);
+
 		pipe(fd[idx]);
-		pid = fork();
+		curr = cmd;
+		if(is_redirection(cmd->next_flag))
+		{
+			while(is_redirection(cmd->next_flag))
+			{
+				check_redirection(cmd, &fd_in, &fd_out);
+				cmd = cmd->next;
+			}
+		}
+		// check_redirection(cmd, &fd_in, &fd_out);
+		if (cmd->next_flag )
+			pid = fork();
 		if(pid == 0)
 		{
-			if(cmd->pre_flag == PIPE)
-				dup2(fd[idx-1][0], 0);
-			if(cmd->next_flag == PIPE)
-				dup2(fd[idx][1], 1);
 			if (fd_in != 0)
-				dup2(0, fd_in);
+				dup2(fd_in, 0);
 			if (fd_out != 1)
-				dup2(1, fd_out);
-			if (exec_built_in_func(cmd->argv, env_lst) == 1)
+				dup2(fd_out, 1);
+			if(cmd->pre_flag == PIPE)
+				dup2(fd[idx-1][0], fd_in);
+			if(cmd->next_flag == PIPE)
+				dup2(fd[idx][1], fd_out);
+			if (exec_built_in_func(curr->argv, env_lst) == 1)
 			{
-				path = path_is_valid(cmd->argv[0], path_arr);
+				path = path_is_valid(curr->argv[0], path_arr);
 				env_arr = make_env_arr(*env_lst);
-				exec_path(path, cmd->argv, env_arr, &fd_in, &fd_out);
+				exec_path(path, curr->argv, env_arr, &fd_in, &fd_out);
 				free_memory(env_arr);
 			}
 		}
@@ -97,14 +118,20 @@ int	minishell(char *envp[])
 	{
 		handle_signal();
 		input = readline("minishell42 $ ");
-		if (!input)
+		/*if (!input)
 		{
 			input = ft_strdup("exit");
 			printf("%s\n", input);
 			free(input);
 			mini_exit();
+		}*/
+		if (ft_strcmp("", input) == 0)
+		{
+			free(input);
+			continue ;
 		}
 		cmd = first_parsing(input, env_lst);
+		printf("cmd-> %s\n",cmd->argv[0]);
 		exec_func(cmd, &env_lst);
 		add_history(input);
 		free(input);
