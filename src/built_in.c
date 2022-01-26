3057981167fce543c4beae8cd8dd1bc8dba0be8d
seongjun kim <tjdwns5063@naver.com>
2022-01-27 01:40:44 +0900

    built-in return값 수정

--- src/built_in.c
+++ src/built_in.c
@@ -61,8 +61,7 @@ static void	exec_fork_case(t_cmd *cmd, t_lst **env_lst, t_cmd *prev)
 	else if (pid > 0)
 	{
 		waitpid(pid, &g_status, 0);
-		// if (!WIFSIGNALED(g_status))
-			g_status = WEXITSTATUS(g_status);
+		g_status = WEXITSTATUS(g_status);
 		if (prev)
 			close(prev->pipe[0]);
 		close(cmd->pipe[1]);
